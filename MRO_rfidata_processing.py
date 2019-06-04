5e5b2ea1813d82f2c827afee73eb75d204e2acb8
fdivruno <f.divruno@skatelescope.org>
2019-06-04 15:13:08 +0100

    Update MRO_rfidata_processing.py

--- MRO_rfidata_processing.py
+++ MRO_rfidata_processing.py
@@ -32,7 +32,8 @@ if read_files==2:
     print('reading files...')
     [freq,data] = RFI.read_MRO_data(indir,outdir)
     # Save the full data loaded in one file:
-    np.savez_compressed(outdir + r'MRO_rfidata_full', freq=freq, data=data)
+    if int(print('Save npz file?\nYes(1) No(0): ')):
+        np.savez_compressed(outdir + r'MRO_rfidata_full', freq=freq, data=data)
     
 elif read_files==1:
     try:
