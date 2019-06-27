868246f1e69f0f19778d7c391a14b54f6e198b03
G. Hovey <37883626+GaryHovey@users.noreply.github.com>
2019-06-27 14:26:57 +0100

    Update General.py
    Added load data from matlab routines

--- rfiLib/General.py
+++ rfiLib/General.py
@@ -4,6 +4,7 @@ Created on Fri Jun 21 00:04:14 2019
     Collection of functions 
 
 @author: f.divruno
+@revised: G. Hovey; added coded to save and load different antL data products
 """
 
 import scipy.io as sio
@@ -18,7 +19,7 @@ def Coord_to_nparray(coord):
     return A
 
 def saveAntInData(antL, filePrefixName):
-    """ Saves data to MATLAB file with vectors named 'time' & 'signal' """
+    """ Saves antenna data for each antenna in antL to MATLAB file named filePrefixName + antL[0].Name"""
     antDict = {}
     for ant in antL:
         antDict['height'] = ant.height
@@ -35,7 +36,7 @@ def saveAntInData(antL, filePrefixName):
         sio.savemat(filePrefixName + 'AntIn' + ant.Name.capitalize(), antDict)
 
 def saveAdcInData(antL, filePrefixName):
-    """ Saves data to MATLAB file with vectors named 'time' & 'signal' """
+    """ Saves adc input data for each antenna in antL to MATLAB file named filePrefixName + antL[0].Name"""
     antDict = {}
     for ant in antL:
         antDict['height'] = ant.height
@@ -55,7 +56,7 @@ def saveAdcInData(antL, filePrefixName):
         sio.savemat(filePrefixName + 'AdcIn' + ant.Name.capitalize(), antDict)
 
 def saveAdcOutData(antL, filePrefixName):
-    """ Saves data to MATLAB file with vectors named 'time' & 'signal' """
+    """ Saves adc output data for each antenna in antL to MATLAB file named filePrefixName + antL[0].Name"""
     antDict = {}
     for ant in antL:
         antDict['height'] = ant.height
@@ -67,14 +68,16 @@ def saveAdcOutData(antL, filePrefixName):
         antDict['SampleRate'] = ant.SampleRate
         antDict['Duration'] = ant.Duration
         antDict['time'] = ant.time
-        antDict['ADC_input_rx'] = ant.ADC_output_rx
-        antDict['ADC_input_sky'] = ant.ADC_output_sky
+        antDict['ADC_output_rx'] = ant.ADC_output_rx
+        antDict['ADC_output_sky'] = ant.ADC_output_sky
 
 #        sio.savemat(filename+'_'+str(i), {"time":Telescope_list[i].time,"ADC_output_rx":Telescope_list[i].ADC_output_rx,"ADC_output_sky":Telescope_list[i].ADC_output_sky })
         # instead of saving the time vector save the sample rate, the time vector can be calculated.
         sio.savemat(filePrefixName + 'AdcOut' + ant.Name.capitalize(), antDict)
         
 def loadAntInData(antL, filePrefixName):
+    """Load antenna data for each antenna in antL from MATLAB file named filePrefixName + antL[0].Name"""
+
     antDict = {}
     for ant in antL:
         antDict = sio.loadmat(filePrefixName + 'AntIn' + ant.Name.capitalize())
@@ -89,4 +92,37 @@ def loadAntInData(antL, filePrefixName):
         ant.sky_source_rx = antDict['sky_source_rx'][0]
         ant.Rx_signal = antDict['Rx_signal'][0]
     return antL
+
+def loadAdcInData(antL, filePrefixName):
+    """Load ADC input data for each antenna in antL from MATLAB file named filePrefixName + antL[0].Name"""
+    antDict = {}
+    for ant in antL:
+        antDict = sio.loadmat(filePrefixName + 'AntIn' + ant.Name.capitalize())
+        ant.height = antDict['height'][0][0]
+        ant.Name = antDict['Name'][0]
+        ant.lat = antDict['lat'][0][0]*u.deg
+        ant.lon = antDict['lon'][0][0]*u.deg
+        ant.Pointing = dict(Elev=antDict['El'][0][0]*u.deg,Azimuth=antDict['Az'][0][0]*u.deg)
+        ant.SampleRate = antDict['SampleRate'][0][0]
+        ant.Duration = antDict['Duration'][0][0]
+        ant.time = antDict['time'][0]
+        ant.ADC_input_rx =antDict['ADC_input_rx'][0]
+        ant.ant.ADC_input_sky = antDict['ADC_input_sky'][0]
+    return antL
         
+def loadAdcOutData(antL, filePrefixName):
+    """Load ADC output data for each antenna in antL from MATLAB file named filePrefixName + antL[0].Name"""
+    antDict = {}
+    for ant in antL:
+        antDict = sio.loadmat(filePrefixName + 'AntIn' + ant.Name.capitalize())
+        ant.height = antDict['height'][0][0]
+        ant.Name = antDict['Name'][0]
+        ant.lat = antDict['lat'][0][0]*u.deg
+        ant.lon = antDict['lon'][0][0]*u.deg
+        ant.Pointing = dict(Elev=antDict['El'][0][0]*u.deg,Azimuth=antDict['Az'][0][0]*u.deg)
+        ant.SampleRate = antDict['SampleRate'][0][0]
+        ant.Duration = antDict['Duration'][0][0]
+        ant.time = antDict['time'][0]
+        ant.ADC_output_rx =antDict['ADC_output_rx'][0]
+        ant.ant.ADC_output_sky = antDict['ADC_output_sky'][0]
+    return antL
