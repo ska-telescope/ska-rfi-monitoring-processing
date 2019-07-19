852601c5fe13d0e4ae9cf72c57104d8b8c2e99c4
fdivruno <f.divruno@skatelescope.org>
2019-07-19 11:06:03 +0100

    Update plot_percentile.py

--- rfiLib/plot_percentile.py
+++ rfiLib/plot_percentile.py
@@ -8,20 +8,24 @@ Created on Thu Apr 11 12:26:56 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_percentile(freq,data,percentile,outdir,dataunits='dBm',title=''):
+def plot_percentile(freq,data,percentile,outdir='',dataunits='dBm',title='',fig=[]):
     #the data is m x n and the m dimension will be used to calculate the percentile, so 
     #this must be the time dimension.
 #    data_perc = np.percentile(data,percentile,axis=0)
     data_sorted = np.sort(data,0)
     index = int((len(data)-1)*percentile/100)
     data_perc = data_sorted[index] 
-    plt.figure(figsize=(30,20))
+    try:
+        plt.figure(fig)
+    except:
+        plt.figure(figsize=(30,20))    
     if dataunits == 'dBm':
         plt.plot(freq,(data_perc), linewidth=2)
     else:
         plt.plot(freq,10*np.log10(data_perc), linewidth=2)
     plt.grid(True,'both')
     plt.title(str(percentile) + ' percentile - ' + title)
-    plt.savefig(outdir + str(percentile) + ' percentile - ' + title, dpi=600, bbox_inches='tight')
+    if outdir!='':
+        plt.savefig(outdir + str(percentile) + ' percentile - ' + title, dpi=600, bbox_inches='tight')
     return data_perc
     
