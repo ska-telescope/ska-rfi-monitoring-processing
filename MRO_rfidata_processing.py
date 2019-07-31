14faf5d874dc53b5ffd35655f9f29ae117f19d13
fdivruno <f.divruno@skatelescope.org>
2019-07-31 02:46:23 +0100

    Update MRO_rfidata_processing.py
    Modified the occupancy calculation

--- MRO_rfidata_processing.py
+++ MRO_rfidata_processing.py
@@ -23,6 +23,91 @@ import subprocess
 
 
 
+maskFreq =[20, 22, 25, 27.5, 31, 34.1, 37.2, 47.2,
+           55.8, 59.5, 60, 70, 75.6, 79, 80, 83, 87,
+           92, 94, 103, 110, 117, 119.5, 126, 130.8,
+           136, 139.25, 140, 144, 149, 151, 155, 159,
+           159.4, 162, 163, 165, 167, 169, 171, 173,
+           175, 177, 179, 181, 183, 185, 187,189, 191, 
+           193, 195, 197, 199, 201, 203, 205, 207, 209,
+           211, 213, 215, 217, 219, 221, 223, 225, 227,
+           229, 231, 233, 235, 237, 239,240, 242,246,
+           253, 259, 264, 269.2, 271, 272, 274, 276,
+           278, 280, 282, 284, 286, 288, 290, 292, 294,
+           296, 298, 300, 302, 304, 306, 308, 310, 312,
+           314, 316, 318, 320, 322, 324, 326, 328, 330,
+           332, 334, 336, 338, 340, 342, 344, 346, 348,
+           350, 352, 354, 356, 358, 365, 370, 375, 380,
+           382, 384, 386, 388, 390, 392, 394, 396, 398,
+           400]
+
+def occupancy(freq,maskFreq,D,sigma_mult,plot_figs =0):
+    '''
+        Calculates the percentage of the time that the power in a certain frequency is
+        above a threshold.
+        inputs:
+                freq:
+                maskFreqs: array with frequencies without RFI as closely spaced as possible.
+                D:
+                sigma_mult:
+                plot_figs:
+    
+    '''
+    
+    nTime = np.size(D,0)
+    nFreq = len(freq)
+    ave = np.average(D,axis=0)
+    sigma = np.std(D,axis=0)
+    occupancy = np.zeros(nFreq)
+    mask_ave = np.interp(maskFreq,freq,ave)
+    mask_ave = np.interp(freq,maskFreq,mask_ave)
+    mask_sigma = np.interp(maskFreq,freq,sigma)
+    mask_sigma = np.interp(freq,maskFreq,mask_sigma)
+    
+    
+    
+    mask = mask_ave+mask_sigma*sigma_mult
+    
+    #for each timestep the flags are calculated and added to the occupancy vector.
+    navg = 10
+    nTime = int(nTime/navg) # averages every 10 time samples
+    for i in range(nTime):
+        D_avg = np.average(D[i*navg:(i+1)*navg,:],0)
+        occupancy += np.array(D_avg > mask).astype('int')
+        
+    # in case needed the acerage plot and the mask can be plotted.
+    # also plots the occupancy as final result.
+    if plot_figs:
+        plt.figure(figsize = [15,10])
+        ax = plt.axes()
+        ax.plot(freq,10*np.log10(mask_sigma),label = 'mask')
+        ax.plot(freq,10*np.log10(mask_sigma*sigma_mult),label = 'mask * multiplier')
+        plt.legend()
+        plt.xlabel('Freq Mhz')
+        plt.ylabel('log ADC units')
+        
+        plt.figure(figsize = [15,10])
+        ax = plt.axes()
+        ax.plot(freq,10*np.log10(mask),label = 'mask')
+        ax.plot(freq,10*np.log10(ave),label = 'average')
+        plt.legend()
+        plt.xlabel('Freq Mhz')
+        plt.ylabel('log ADC units')
+        
+        plt.figure(figsize = [15,10])
+        ax = plt.axes()
+        ax.plot(freq,occupancy*100/nTime)
+        plt.title('occupancy')
+        plt.xlabel('Freq Mhz')
+        plt.ylabel('occupancy % of time')
+        
+    return occupancy*100/nTime
+
+
+
+
+
+
 # Clear the screen
 os.system('clear')
 
@@ -45,6 +130,7 @@ else:
 # in windows
     indir = 'C:\\Users\\F.Divruno\\Dropbox (SKA)\\14- RFI environment\\01- Australia\\rfidata_mro\\'
     outdir = 'C:\\Users\\F.Divruno\\Dropbox (SKA)\\14- RFI environment\\01- Australia\\rfidata_mro\\results\\'
+
 
 
 #%% Read the files from disk
@@ -180,7 +266,7 @@ while selection != '0':
     if selection == '4': #Average
         print('Calculating Average')
         ave = np.average(D,0)            
-        plt.figure(figsize=(15,10),dpi=600)
+        plt.figure(figsize=(15,10))
         plt.plot(freqs,ave, linewidth=2)
         plt.ylabel('Ampl [dBm]')
         plt.xlabel('freq [MHz]')
@@ -204,7 +290,9 @@ while selection != '0':
     if selection == '6': #Occupancy
         print('Calculating occupancy...')
         title = 'Occupancy_'+ time_freq
-        Normalized, S_occupancy =  RFI.spectral_occupancy(freqs,D,outdir,title,3)
+#        Normalized, S_occupancy =  RFI.spectral_occupancy(freqs,D,outdir,title,3)
+        S_occupancy = occupancy(freqs,maskFreq,10**(D/10),2,plot_figs =1)
+
 #        threshold = int(input('Calculate BW loss greater than (percent of the time):  '))
         threshold = (1,2,5,10,30,50,80,90,100)
         for j in threshold:
