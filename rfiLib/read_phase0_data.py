34132ae26d6b85a5f659e533fc8ada0503ae5038
fdivruno <f.divruno@skatelescope.org>
2019-07-31 02:47:08 +0100

    Update read_phase0_data.py
    Modified to make it more time efficient

--- rfiLib/read_phase0_data.py
+++ rfiLib/read_phase0_data.py
@@ -17,21 +17,29 @@ def read_phase0_data(directory,files_num):
         N_files = np.size(files)
     else:
         N_files = files_num
-    time_vect = np.zeros([N_files])
-    data = np.zeros([N_files,header]).astype('int8')
-    i = 0
+    
+    count =0
+    for i in range(N_files):
+        f = files[i]
+        fullpath = os.path.join(directory, f)
+        if os.path.splitext(fullpath)[1] == '.tdd':        
+            count+=1
+                
+    time_vect = np.zeros([count])
+    data = np.zeros([count,header]).astype('int8')
+    k = 0
     for i in range(N_files):
         f = files[i]
         fullpath = os.path.join(directory, f)
         if os.path.splitext(fullpath)[1] == '.tdd':
            with open(fullpath,'r') as f:
                header = np.fromfile(f, dtype=np.dtype('>f8') , count = 1)#dtype=np.floating point 8 bytes
-               data[i,:] = np.fromfile(f, dtype=np.dtype('>i1') , count = 131072)#dtype=np.int64
-        sec = os.path.splitext(fullpath)[0][-2::]
-        mins = os.path.splitext(fullpath)[0][-4:-2]
-        hours = os.path.splitext(fullpath)[0][-6:-4]
-        time_vect[i] = int(hours)*60*60+int(mins)*60+int(sec)
-        i += 1
-        print(str(i))
+               data[k,:] = np.fromfile(f, dtype=np.dtype('>i1') , count = 131072)#dtype=np.int64
+           sec = os.path.splitext(fullpath)[0][-2::]
+           mins = os.path.splitext(fullpath)[0][-4:-2]
+           hours = os.path.splitext(fullpath)[0][-6:-4]
+           time_vect[k] = int(hours)*60*60+int(mins)*60+int(sec)
+           k += 1
+        print('file ' + str(i) + 'of' + str(N_files))
         
     return time_vect,data
